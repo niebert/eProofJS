8c551aa3b1afd99fbf449c2d792b0c9fc1de02ad
Bert Niehaus <niehausbert@gmail.com>
2016-07-24 21:07:54 +0200

    Clear LocalStorage for ResetProof added

---
+++ demo/library/js2electron.js
@@ -0,0 +1,11 @@
+//#################################################################
+//# Javascript Class:         EProof Electron  Function Mapper
+//#   Method Definition File: js2electron.js
+//#
+//# Author of Class:      Engelbert Niehaus
+//# email:                niehaus@uni-landau.de
+//# created               24.2.2015
+//# last modifications    __DATE__
+//# GNU Public License - OpenSource
+//# created with JavaScript Class Generator by Engelbert Niehaus 
+//#################################################################
