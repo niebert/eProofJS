92d36f7b082936486557f2cfd5803361ae77ecc5
Bert Niehaus <niehausbert@gmail.com>
2016-10-11 22:12:04 +0200

    rename_release.sh added to eProofJS (copied and adapted from PanDocElectron)

---
+++ rename_release.sh
@@ -0,0 +1,49 @@
+AppName="eProofElectron"
+vReleasDir="releases/"
+vVersion=""
+mkdir "${vReleasDir}"
+cd "${vReleasDir}"
+
+rm -R "${AppName}-mas-x64"
+
+vArchOld="x64"
+vArchNew="64Bit"
+echo "Create for Architecture ${vArchNew} (${vArchOld})"
+vOSOld="darwin"
+vOSNew="MacOSX"
+mv "${AppName}-${vOSOld}-${vArchOld}" "${AppName}-${vOSNew}-${vArchNew}${vVersion}"
+zip -r "${AppName}-${vOSNew}-${vArchNew}${vVersion}.zip" "${AppName}-${vOSNew}-${vArchNew}${vVersion}"
+sudo rm -R "${AppName}-${vOSNew}-${vArchNew}${vVersion}"
+
+vOSOld="win32"
+vOSNew="Windows"
+mv "${AppName}-${vOSOld}-${vArchOld}" "${AppName}-${vOSNew}-${vArchNew}${vVersion}"
+zip -r "${AppName}-${vOSNew}-${vArchNew}${vVersion}.zip" "${AppName}-${vOSNew}-${vArchNew}${vVersion}"
+sudo rm -R "${AppName}-${vOSNew}-${vArchNew}${vVersion}"
+
+vOSOld="linux"
+vOSNew="Linux"
+mv "${AppName}-${vOSOld}-${vArchOld}" "${AppName}-${vOSNew}-${vArchNew}${vVersion}"
+zip -r "${AppName}-${vOSNew}-${vArchNew}${vVersion}.zip" "${AppName}-${vOSNew}-${vArchNew}${vVersion}"
+sudo rm -R "${AppName}-${vOSNew}-${vArchNew}${vVersion}"
+
+vArchOld="ia32"
+vArchNew="32Bit"
+echo "Create for Architecture ${vArchNew} (${vArchOld})"
+mv "${AppName}-${vOSOld}-${vArchOld}" "${AppName}-${vOSNew}-${vArchNew}${vVersion}"
+zip -r "${AppName}-${vOSNew}-${vArchNew}${vVersion}.zip" "${AppName}-${vOSNew}-${vArchNew}${vVersion}"
+sudo rm -R "${AppName}-${vOSNew}-${vArchNew}${vVersion}"
+
+vOSOld="win32"
+vOSNew="Windows"
+mv "${AppName}-${vOSOld}-${vArchOld}" "${AppName}-${vOSNew}-${vArchNew}${vVersion}"
+zip -r "${AppName}-${vOSNew}-${vArchNew}${vVersion}.zip" "${AppName}-${vOSNew}-${vArchNew}${vVersion}"
+sudo rm -R "${AppName}-${vOSNew}-${vArchNew}${vVersion}"
+
+vOSOld="linux"
+vOSNew="Linux"
+mv "${AppName}-${vOSOld}-${vArchOld}" "${AppName}-${vOSNew}-${vArchNew}${vVersion}"
+zip -r "${AppName}-${vOSNew}-${vArchNew}${vVersion}.zip" "${AppName}-${vOSNew}-${vArchNew}${vVersion}"
+sudo rm -R "${AppName}-${vOSNew}-${vArchNew}${vVersion}"
+
+cd ..
